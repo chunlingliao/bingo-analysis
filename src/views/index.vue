39220513b24a1994fc0ef2f463fc4055d662ffd9
dabbie <dabbie@mail.js-tech.tw>
2020-12-08 15:52:32 +0800

    添加 Go to Top 功能與樣式

--- src/views/index.vue
+++ src/views/index.vue
@@ -240,10 +240,12 @@
       </div>
     </div>
   </div>
+  <a href="#" class="toTop" title="GoTop" style="display: inline;"><span></span></a>
 </div>
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
   name: "",
   props: {},
@@ -289,6 +291,7 @@ export default {
   },
   methods: {
     // 初始
+     
   },
   //BEGIN--生命週期
   beforeCreate: function() {
@@ -304,6 +307,20 @@ export default {
   mounted: function() {
     //元素已掛載， $el 被建立。
     console.log(window.customElements)
+    //gotop
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('.toTop').fadeIn()
+      } else {
+        $('.toTop').fadeOut()
+      }
+    })
+    $('.toTop').click(function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800)
+      return false
+    })
   },
   beforeUpdate: function() {
     //當資料變化時被呼叫，還不會描繪 View。
