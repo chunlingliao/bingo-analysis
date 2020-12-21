8843f3348b1a17f8f4c198a7f8e09fe2a6357e58
dabbie <dabbie@mail.js-tech.tw>
2020-12-21 18:38:50 +0800

    歷史開獎紀錄頁面 資料整理
    把資料用迴圈跑＆配合後端api的參數

--- src/views/index.vue
+++ src/views/index.vue
@@ -17,10 +17,10 @@
       </div>
       
       <div class="col-sm-5">
-        <div class="font-weight-bold mb-3 mt-3 mt-sm-0">第123456789期</div>
+        <div class="font-weight-bold mb-3 mt-3 mt-sm-0">第{{ history[0].draw_number }}期</div>
           <ul class="nav number-periods-lottery">
-            <li class="l-bingo-number number-inplay">11</li>
-            <li class="l-bingo-number number-inplay">11</li>
+            <li v-for="balls in history[0].balls" :key="balls" class="l-bingo-number number-inplay">{{ balls }}</li>
+            <!-- <li class="l-bingo-number number-inplay">11</li>
             <li class="l-bingo-number number-inplay">11</li>
             <li class="l-bingo-number number-inplay">11</li>
             <li class="l-bingo-number number-inplay">11</li>
@@ -38,44 +38,34 @@
             <li class="l-bingo-number number-inplay">11</li>
             <li class="l-bingo-number number-inplay">11</li>
             <li class="l-bingo-number number-inplay">11</li>
-            <li class="l-bingo-number number-inplay">11</li>
+            <li class="l-bingo-number number-inplay">11</li> -->
           </ul>
       </div>
 
       <div class="col-sm-2 col-6 mt-3 mt-sm-0">
         <div class="font-weight-bold mb-2">超級獎號</div>
-        <div class="large-super-number">45</div>
+        <div class="large-super-number">{{ history[0].super_ball }}</div>
       </div>
       <div class="col-sm-2 col-6 mt-3 mt-sm-0">
         <div class="font-weight-bold mb-3">獎號總和</div>
-        <div class="text-primary number-total font-weight-bold">118</div>
+        <div class="text-primary number-total font-weight-bold">{{ history[0].total_number }}</div>
       </div>
     </div>
 
     <!-- 第二塊預期總和 -->
     <div class="expected-sum mb-3">
-      <div class="text-left float-left expected-sum-text font-weight-bold">109067899898期預測總和 小</div>
+      <div class="text-left float-left expected-sum-text font-weight-bold">
+        {{ estimate.draw_number }}期預測總和 {{ estimate.even }} {{ estimate.size }}
+        <!-- <span v-if="estimate.total_number <= '810'">
+          小
+        </span>
+        <span v-else>
+          大
+        </span> -->
+      </div>
         <ul class="nav">
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number super-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
-          <li class="l-bingo-number">11</li>
+          <li v-for="balls in estimate.balls" :key="balls" 
+            :class="['l-bingo-number',{'super-number': Number(estimate.super_ball) === Number(balls)}]">{{ balls }}</li>
         </ul>
     </div>
 
@@ -106,98 +96,38 @@
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td class="font-weight-bold period">第111111111期</td>
+                <tr v-for="item in history" :key="item">
+                  <td class="font-weight-bold period">
+                    第{{ item.draw_number }}期</td>
                   <td>
                     <ul class="nav d-flex justify-content-center win-number history-lottery">
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number super-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[0])}]">{{ item.balls[0] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[1])}]">{{ item.balls[1] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[2])}]">{{ item.balls[2] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[3])}]">{{ item.balls[3] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[4])}]">{{ item.balls[4] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[5])}]">{{ item.balls[5] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[6])}]">{{ item.balls[6] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[7])}]">{{ item.balls[7] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[8])}]">{{ item.balls[8] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[9])}]">{{ item.balls[9] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[10])}]">{{ item.balls[10] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[11])}]">{{ item.balls[11] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[12])}]">{{ item.balls[12] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[13])}]">{{ item.balls[13] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[14])}]">{{ item.balls[14] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[15])}]">{{ item.balls[15] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[16])}]">{{ item.balls[16] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[17])}]">{{ item.balls[17] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[18])}]">{{ item.balls[18] }}</li>
+                      <li :class="['l-bingo-number',{'super-number': Number(item.super_ball) === Number(item.balls[19])}]">{{ item.balls[19] }}</li>
+                      <!-- <li class="l-bingo-number super-number">11</li> -->
                     </ul>
                   </td>
                   <td>
-                    <div class="history-super-number">45</div>
+                    <div class="history-super-number">{{ item.super_ball }}</div>
                   </td>
-                  <td><div class="history-number-total">118</div></td>
-                </tr>
-                <tr>
-                  <td class="font-weight-bold period">第222222222期</td>
-                  <td>
-                    <ul class="nav d-flex justify-content-center win-number history-lottery">
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number super-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                    </ul>
-                  </td>
-                  <td>
-                    <div class="history-super-number">45</div>
-                  </td>
-                  <td><div class="history-number-total">118</div></td>
-                </tr>
-                <tr>
-                  <td class="font-weight-bold period">第333333333期</td>
-                  <td>
-                    <ul class="nav d-flex justify-content-center win-number history-lottery">
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number super-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                      <li class="l-bingo-number">11</li>
-                    </ul>
-                  </td>
-                  <td>
-                    <div class="history-super-number">45</div>
-                  </td>
-                  <td><div class="history-number-total">118</div></td>
+                  <td><div class="history-number-total">{{ item.total_number }}</div></td>
                 </tr>
               </tbody>
             </table>
@@ -246,6 +176,40 @@ export default {
   data: function() {
     // 資料
     return {
+      // 預測號碼
+      estimate:
+        {       
+          "balls": [1, 3, 7, 12, 17, 19, 20, 31, 33, 34, 38, 43, 45, 47, 48, 63, 68, 69, 71, 78],
+          "draw_number": "109069700",
+          "super_ball": "7",
+          "even":"單",
+          "size":'大',
+          "total_number": "811"
+        },
+      // 歷史號碼
+      history:[
+        {
+          "balls": [1, 3, 7, 12, 17, 19, 20, 31, 33, 34, 38, 43, 45, 47, 48, 63, 68, 69, 71, 78],
+          "draw_number": "109069751",
+          "game_time": "Wed, 09 Dec 2020 17:10:00 GMT",
+          "super_ball": "1",
+          "total_number": "817"
+        },
+        {
+          "balls": [1, 3, 7, 12, 17, 19, 20, 31, 33, 34, 38, 43, 45, 47, 48, 63, 68, 69, 71, 78],
+          "draw_number": "109069752",
+          "game_time": "Wed, 09 Dec 2020 17:10:00 GMT",
+          "super_ball": "3",
+          "total_number": "347"
+        },
+        {
+          "balls": [1, 3, 7, 12, 17, 19, 20, 31, 33, 34, 38, 43, 45, 47, 48, 63, 68, 69, 71, 78],
+          "draw_number": "109069753",
+          "game_time": "Wed, 09 Dec 2020 17:10:00 GMT",
+          "super_ball": "78",
+          "total_number": "555"
+        }
+      ],
       hotNumber:[
         [{ ball:'01', type:'10'},{ ball:'01', type:'10'},{ ball:'01', type:'10'},
         { ball:'01', type:'10'},{ ball:'01', type:'10'},{ ball:'01', type:'10'},
