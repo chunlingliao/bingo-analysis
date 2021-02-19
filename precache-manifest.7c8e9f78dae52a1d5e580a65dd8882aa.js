fc554420f49a1a03c523f8ee8874b27e037efc67
dabbie <dabbie@mail.js-tech.tw>
2021-02-19 18:17:30 +0800

    gh-pages部署到線上

---
+++ precache-manifest.7c8e9f78dae52a1d5e580a65dd8882aa.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dcfcc3d11ba5471705",
+    "url": "/bingo-analysis/css/app.53fbc74f.css"
+  },
+  {
+    "revision": "ac2470567fca16f4e0ebc1292ef737f8",
+    "url": "/bingo-analysis/css/app.css"
+  },
+  {
+    "revision": "7a4672e0f17ea5adaf04",
+    "url": "/bingo-analysis/css/chunk-f19c997c.71a233fb.css"
+  },
+  {
+    "revision": "e53c504fa312e477d29c",
+    "url": "/bingo-analysis/css/chunk-vendors.26c65960.css"
+  },
+  {
+    "revision": "a33bbca37391acac370838da4086b6c9",
+    "url": "/bingo-analysis/images/ball01.png"
+  },
+  {
+    "revision": "d07c9eff221bd0c3a481612edb2bbeff",
+    "url": "/bingo-analysis/images/ball02.png"
+  },
+  {
+    "revision": "f65a9c5950b50b59bc654fc939683704",
+    "url": "/bingo-analysis/images/ball03.png"
+  },
+  {
+    "revision": "5dd4af70a7a28a21c2c89955a6c0e5da",
+    "url": "/bingo-analysis/images/ball04.png"
+  },
+  {
+    "revision": "5762795f79859b9fcb97319a5479af72",
+    "url": "/bingo-analysis/images/ball05.png"
+  },
+  {
+    "revision": "2798401c3f8ac80a99509cb411e2a9ff",
+    "url": "/bingo-analysis/images/ball06.png"
+  },
+  {
+    "revision": "2edf9621bca224e64d25aeffaa9ad4ff",
+    "url": "/bingo-analysis/images/ball07.png"
+  },
+  {
+    "revision": "b9adac625121acf83fec00dbe8c0ac67",
+    "url": "/bingo-analysis/images/ball08.png"
+  },
+  {
+    "revision": "fd4182b23bb0cde47debfff1b9c4300d",
+    "url": "/bingo-analysis/images/ball09.png"
+  },
+  {
+    "revision": "7048ba7d6511cd5700845c31fce980a7",
+    "url": "/bingo-analysis/images/login-logo.png"
+  },
+  {
+    "revision": "344f9061c8ed9000d155382863dd7ae5",
+    "url": "/bingo-analysis/images/login.png"
+  },
+  {
+    "revision": "344f9061c8ed9000d155382863dd7ae5",
+    "url": "/bingo-analysis/img/login.344f9061.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/bingo-analysis/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9f4a568da54ada193be8f8a8de27eaf4",
+    "url": "/bingo-analysis/index.html"
+  },
+  {
+    "revision": "d4dcfcc3d11ba5471705",
+    "url": "/bingo-analysis/js/app.8e606fad.js"
+  },
+  {
+    "revision": "7b23b0aa46b4793a5fdf",
+    "url": "/bingo-analysis/js/chunk-2af5d52d.7a2817dc.js"
+  },
+  {
+    "revision": "7f7ed8f538d58c5ed405",
+    "url": "/bingo-analysis/js/chunk-62a2b5de.6ea89740.js"
+  },
+  {
+    "revision": "7a4672e0f17ea5adaf04",
+    "url": "/bingo-analysis/js/chunk-f19c997c.621dbded.js"
+  },
+  {
+    "revision": "e53c504fa312e477d29c",
+    "url": "/bingo-analysis/js/chunk-vendors.0163db05.js"
+  },
+  {
+    "revision": "fcc4126662aa75f64f359a88d2d76ed9",
+    "url": "/bingo-analysis/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bingo-analysis/robots.txt"
+  }
+]);
